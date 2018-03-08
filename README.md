bce1a688fb272c8cff9718d2f54127397d16bdfc
George Zhang <275132122@qq.com>
2018-03-08 14:18:04 +0800

    初始提交

---
+++ README.md
@@ -0,0 +1,5 @@
+#订票系统的前端部分
+技术栈:
+- react
+- mobx
+- styled-components
---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+class App extends React.Component {
+    render() {
+        // todo
+        return null;
+    }
+}
+
+export default App;
